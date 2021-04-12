2f51143bca2e071b313d3a8fc5cb637d306fc55b
zeeshanyousaf93 <zeeshanyousaf93@gmail.com>
2021-04-12 19:36:23 -0400

    Function added for changing bg color

--- js/script.js
+++ js/script.js
@@ -2,13 +2,12 @@
 // If no argument is passed, 256 is set, ie, returns a number between and including 0 to 255
 // 		For example: 
 //    getRandomColorChannel(5) will return either 0, 1, 2, 3, 4
-const getRandomColorChannel = function(options=256) {
-  return Math.floor( Math.random() * options )
+const getRandomColorChannel = function (options = 256) {
+  return Math.floor(Math.random() * options)
 }
 
 // Here's a test number, check the console to see the result
-console.log( getRandomColorChannel() )
-
+console.log(getRandomColorChannel())
 
 
 // COMPLETE THE FOLLOWING STEPS
@@ -20,3 +19,12 @@ console.log( getRandomColorChannel() )
 // 3) Use the three individual RGB numbers to update the HTML element "rgb(###, ####, ###)" (replacing ###s with the values)
 
 // 4) Wrap the entire procedure in a function named `setNewColour()`, call it multiple times from the `console` to test
+
+const setNewColour = function () {
+  var num1 = getRandomColorChannel();
+  var num2 = getRandomColorChannel();
+  var num3 = getRandomColorChannel();
+  document.getElementsByTagName('body')[0].style.backgroundColor = `rgb(${num1},${num2},${num3})`;
+}
+
+setNewColour();
